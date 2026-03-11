f1fc955665b9058325b81dd38dc3f4959c8a8ca4
nhox <nox@gmaail.com>
2026-03-11 15:35:28 +0300

    mise a jour

--- telecharge.py
+++ telecharge.py
@@ -2,16 +2,16 @@ import yt_dlp
 import os
 
 def telecharger_media():
-    # 1. Demander le lien à l'utilisateur
+    
     url = input("Colle l'URL de la vidéo YouTube : ")
     
-    # 2. Demander le format
+   
     print("\nQue veux-tu faire ?")
     print("1. Télécharger l'AUDIO (MP3)")
     print("2. Télécharger la VIDÉO (MP4)")
     choix = input("Choisis 1 ou 2 : ")
 
-    # 3. Configuration des options yt-dlp
+   
     if choix == '1':
         ydl_opts = {
             'format': 'bestaudio/best',
@@ -20,16 +20,15 @@ def telecharger_media():
                 'preferredcodec': 'mp3',
                 'preferredquality': '192',
             }],
-            'outtmpl': 'Telechargements/Musique/%(title)s.%(ext)s', # Dossier spécifique
+            'outtmpl': 'Telechargements/Musique/%(title)s.%(ext)s',
         }
     else:
         ydl_opts = {
-            # On demande un format qui contient DEJA la vidéo et l'audio ensemble
             'format': 'best[ext=mp4]/best', 
             'outtmpl': 'Telechargements/Videos/%(title)s.%(ext)s',
         }
 
-    # 4. Exécution du téléchargement
+   
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             print("\nInitialisation... (ça peut prendre quelques secondes)")
@@ -39,7 +38,7 @@ def telecharger_media():
         print(f"Oups, une erreur est survenue : {e}")
 
 if __name__ == "__main__":
-    # Créer les dossiers s'ils n'existent pas
+    
     if not os.path.exists('Telechargements'):
         os.makedirs('Telechargements/Musique')
         os.makedirs('Telechargements/Videos')
